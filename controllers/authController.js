df026628bec78d65923dfd49f1109eaa3d3b22d4
Vuthy <suonx002@gmail.com>
2020-06-15 02:01:30 -0500

    setting up protectedRoute and handling jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
@@ -6,6 +8,16 @@ const authValidator = require('../validators/authValidator');
 
 const db = require('../db/db');
 const tableNames = require('../db/tableNames');
+
+const signJwtToken = (email) => {
+  const payload = { email };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+
+  return token;
+};
 
 const register = catchAsync(async (req, res, next) => {
   // check for body validation
@@ -32,8 +44,11 @@ const register = catchAsync(async (req, res, next) => {
     })
     .returning('*');
 
+  const token = signJwtToken(email);
+
   res.status(200).json({
     status: 'success',
+    token,
     data: createUser,
   });
 });
@@ -62,13 +77,51 @@ const login = catchAsync(async (req, res, next) => {
   // hide password
   user[0].password = undefined;
 
+  const token = signJwtToken(email);
+
   res.status(200).json({
     status: 'success',
+    token,
     data: user,
   });
+});
+
+const protectRoute = catchAsync(async (req, res, next) => {
+  let token;
+
+  // getting token and check if it's there
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('Please log in to get access', 401));
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await db(tableNames.users)
+    .where({
+      email: decoded.email,
+    })
+    .select('email');
+
+  if (currentUser.length === 0) {
+    return next(
+      new AppError('The token belonging to this user is no longer exist', 401)
+    );
+  }
+
+  req.user = currentUser[0];
+
+  next();
 });
 
 module.exports = {
   register,
   login,
+  protectRoute,
 };
